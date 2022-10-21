b941ae68296b833dfa2b105d091e8d264f078a78
DenisSofronov <sofronovd1@yandex.ru>
2022-10-21 13:34:17 +0500

    commit

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,24 +1,22 @@
 const modal = document.getElementById("modal_main");
-modal.classList.add("modal_active");
-
-const close = document.querySelector(".modal__close");
+const secondModal = document.getElementById("modal_success");
+const close = document.querySelectorAll(".modal__close");
 const openSecondModal = document.querySelector(".show-success");
 
-close.onclick = function() {
-  modal.style.display = "none";
-};
+modal.classList.add("modal_active");
+
+for (button of close) {
+  button.onclick = function() {
+    modal.style.display = "none";
+    secondModal.style.display = "none";
+  };
+}
 
 openSecondModal.onclick = function() {
   modal.style.display = "none";
-  const secondModal = document.getElementById("modal_success");
   secondModal.classList.add("modal_active");
 
-  const closeAll = document.querySelector(".modal__close");
   const closeSecondModal = document.querySelector(".btn_success");
-
-  closeAll.onclick = function() {
-    secondModal.style.display = "none";
-  };
 
   closeSecondModal.onclick = function() {
     secondModal.style.display = "none";
