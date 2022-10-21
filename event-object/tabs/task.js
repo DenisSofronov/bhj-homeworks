03b55981fdb20dcf5ec4f924694075b43df7709e
DenisSofronov <sofronovd1@yandex.ru>
2022-10-21 19:06:54 +0500

    Commit tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,36 +1,19 @@
-const tabs = document.querySelectorAll('.tab');
+const tab = document.querySelectorAll('.tab');
+const tabArray = Array.from(tab);
 const tabContent = document.querySelectorAll('.tab__content');
-const tabs1 = document.getElementById('tabs1');
+const tabContentArray = Array.from(tabContent);
+const tabs = document.querySelector('.tabs');
 
-const tabsArr = Array.from(tabs);
-
-tabs1.onclick = function(e) {
-  const tabIndex = tabsArr.indexOf(e.target);
-
-  if (tabIndex === 0) {
-    tabs[tabIndex].classList.add('tab_active');
-    tabs[tabIndex + 1].classList.remove('tab_active');
-    tabs[tabIndex + 2].classList.remove('tab_active');
-    tabContent[tabIndex].classList.add('tab__content_active');
-    tabContent[tabIndex + 1].classList.remove('tab__content_active');
-    tabContent[tabIndex + 2].classList.remove('tab__content_active');
-  }
-
-  if (tabIndex === 1) {
-    tabs[tabIndex].classList.add('tab_active');
-    tabs[tabIndex - 1].classList.remove('tab_active');
-    tabs[tabIndex + 1].classList.remove('tab_active');
-    tabContent[tabIndex].classList.add('tab__content_active');
-    tabContent[tabIndex - 1].classList.remove('tab__content_active');
-    tabContent[tabIndex + 1].classList.remove('tab__content_active');
-  }
-
-  if (tabIndex === 2) {
-    tabs[tabIndex].classList.add('tab_active');
-    tabs[tabIndex - 1].classList.remove('tab_active');
-    tabs[tabIndex - 2].classList.remove('tab_active');
-    tabContent[tabIndex].classList.add('tab__content_active');
-    tabContent[tabIndex - 1].classList.remove('tab__content_active');
-    tabContent[tabIndex - 2].classList.remove('tab__content_active');
-  }
-};
+tabs.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target.classList.contains('tab')) {
+        tabArray.forEach(tab => tab.classList.remove('tab_active'));
+        tabContentArray.forEach(text => text.classList.remove('tab__content_active'));
+        target.classList.add('tab_active');
+    }
+    for (let i = 0; i < tabArray.length; i++) {
+        if (tabArray[i].classList.contains('tab_active')) {
+            tabContentArray[i].classList.add('tab__content_active');
+        }
+    }
+})
